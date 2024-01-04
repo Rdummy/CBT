95a561356770842932c627480f1db08973fe9e79
Kurt Weyne Gaso <94361651+Skamby@users.noreply.github.com>
2024-01-04 15:56:51 +0800

    ExamResultPage toggle Pagination

--- client/src/pages/result-page.jsx
+++ client/src/pages/result-page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import {
   Card,
@@ -6,33 +6,146 @@ import {
   Typography,
   Container,
   Toolbar,
-  Box,
+  Button,
 } from "@mui/material";
 import ReturnDashboard from "../components/ReturnDashboard.jsx";
 import exams from "../models/exam-data";
-import { MdCheck, MdClear } from "react-icons/md"; // Import icons
+import "../assets/styles/ExamResultPage.css";
+
+const itemsPerPage = 1;
 
 const ExamResultPage = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [showResults, setShowResults] = useState(false);
+
   const { score, examId } = useParams();
   const selectedExam = exams.find((exam) => exam.id === examId);
   const { questions } = selectedExam;
 
-  // Retrieve chosen answers from localStorage
   const storedAnswers = JSON.parse(
     localStorage.getItem(`chosenAnswers_${examId}`)
   );
 
-  const getIconForAnswer = (isCorrect) => {
-    return isCorrect ? (
-      <MdCheck style={{ color: "green" }} />
-    ) : (
-      <MdClear style={{ color: "red" }} />
+  const totalPages = Math.ceil(questions.length / itemsPerPage);
+  const offset = currentPage * itemsPerPage;
+  const currentQuestions = questions.slice(offset, offset + itemsPerPage);
+
+  const getResultLabel = () => {
+    if (score !== null) {
+      return parseFloat(score) >= 50 ? "PASSED" : "FAIL";
+    } else {
+      return "";
+    }
+  };
+
+  const getIconForAnswer = (isCorrect, isKnowSection) => {
+    const icon = isCorrect ? "✓" : "✗";
+    const className = isCorrect ? "check-mark" : "cross-mark";
+    return <span className={className}>{icon}</span>;
+  };
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 0 && newPage < totalPages) {
+      setCurrentPage(newPage);
+    }
+  };
+
+  const toggleResults = () => {
+    setShowResults(!showResults);
+  };
+
+  const renderPagination = () => {
+    const pages = [];
+    for (let i = 0; i < totalPages; i++) {
+      pages.push(
+        <button
+          key={i}
+          className={`page ${currentPage === i ? "active" : ""}`}
+          onClick={() => handlePageChange(i)}
+        >
+          {i + 1}
+        </button>
+      );
+    }
+    return (
+      <div className="pagination">
+        {renderPreviousButton()}
+        {pages}
+        {renderNextButton()}
+      </div>
     );
   };
 
-  const getResultLabel = () => {
-    const numericScore = parseFloat(score);
-    return numericScore >= 50 ? "PASSED" : "FAIL";
+  const renderPreviousButton = () => (
+    <button
+      className="previous"
+      onClick={() => handlePageChange(currentPage - 1)}
+      disabled={currentPage === 0}
+    >
+      Previous
+    </button>
+  );
+
+  const renderNextButton = () => (
+    <button
+      className="next"
+      onClick={() => handlePageChange(currentPage + 1)}
+      disabled={currentPage === totalPages - 1}
+    >
+      Next
+    </button>
+  );
+
+  const getReviewQuestions = () => {
+    const reviewQuestions = [];
+    currentQuestions.forEach((question, index) => {
+      const globalIndex = offset + index;
+      if (
+        storedAnswers &&
+        storedAnswers[globalIndex] !== undefined &&
+        question.choices &&
+        storedAnswers[globalIndex] !== question.correctAnswer
+      ) {
+        reviewQuestions.push({
+          index: globalIndex + 1,
+          question: question.question,
+          yourAnswer:
+            storedAnswers[globalIndex] !== undefined
+              ? question.choices[storedAnswers[globalIndex]]
+              : "Not answered",
+          correctAnswer: question.choices[question.correctAnswer],
+          explanation: question.explanation,
+          knowledge: question.knowledge, // added knowledge property
+        });
+      }
+    });
+    return reviewQuestions;
+  };
+
+  const getKnowQuestions = () => {
+    const knowQuestions = [];
+    currentQuestions.forEach((question, index) => {
+      const globalIndex = offset + index;
+      if (
+        storedAnswers &&
+        storedAnswers[globalIndex] !== undefined &&
+        question.choices &&
+        storedAnswers[globalIndex] === question.correctAnswer
+      ) {
+        knowQuestions.push({
+          index: globalIndex + 1,
+          question: question.question,
+          yourAnswer:
+            storedAnswers[globalIndex] !== undefined
+              ? question.choices[storedAnswers[globalIndex]]
+              : "Not answered",
+          correctAnswer: question.choices[question.correctAnswer],
+          explanation: question.explanation,
+          knowledge: question.knowledge, // added knowledge property
+        });
+      }
+    });
+    return knowQuestions;
   };
 
   return (
@@ -59,46 +172,33 @@ const ExamResultPage = () => {
           alignItems: "center",
         }}
       >
-        <Card sx={{ width: "100%", maxWidth: 500 }}>
+        <Card sx={{ width: "100%", maxWidth: 900, marginTop: "2rem" }}>
           <ReturnDashboard />
           <CardContent sx={{ p: "2rem" }}>
             <Typography variant="h4" gutterBottom>
               Exam Result
-              <Typography
-                variant="h6"
-                style={{
-                  color:
-                    score !== undefined
-                      ? parseFloat(score) >= 50
-                        ? "green"
-                        : "red"
-                      : "black",
-                }}
-              >
-                {getResultLabel()}
-              </Typography>
             </Typography>
+
             <Typography variant="h6">
               Your Score: {score !== undefined ? `${score}%` : "Calculating..."}
             </Typography>
 
-            <div>
-              <Typography variant="h5" gutterBottom>
-                Correct and Incorrect Answers:
-              </Typography>
-              {questions.map((question, index) => (
+            <div className={`results-container ${showResults ? "show" : ""}`}>
+              <Typography variant="h5">Questions:</Typography>
+              {currentQuestions.map((question, index) => (
                 <div key={index}>
                   <Typography variant="subtitle1">
-                    Question {index + 1}: {question.question}
+                    Question {offset + index + 1}: {question.question}
                   </Typography>
                   <Typography
                     variant="body1"
                     style={{
                       color:
                         storedAnswers &&
-                        storedAnswers[index] !== undefined &&
+                        storedAnswers[offset + index] !== undefined &&
                         question.choices
-                          ? storedAnswers[index] === question.correctAnswer
+                          ? storedAnswers[offset + index] ===
+                            question.correctAnswer
                             ? "green"
                             : "red"
                           : "black",
@@ -106,54 +206,85 @@ const ExamResultPage = () => {
                   >
                     Your Answer:{" "}
                     {storedAnswers &&
-                    storedAnswers[index] !== undefined &&
+                    storedAnswers[offset + index] !== undefined &&
                     question.choices
-                      ? question.choices[storedAnswers[index]]
+                      ? question.choices[storedAnswers[offset + index]]
                       : "Not answered"}
                     {getIconForAnswer(
-                      storedAnswers && storedAnswers[index] !== undefined
-                        ? storedAnswers[index] === question.correctAnswer
+                      storedAnswers &&
+                        storedAnswers[offset + index] !== undefined
+                        ? storedAnswers[offset + index] ===
+                            question.correctAnswer
                         : false
                     )}
                   </Typography>
                   <Typography variant="body1">
                     Correct Answer: {question.choices[question.correctAnswer]}
                   </Typography>
+                  <Typography variant="body1">
+                    Explanation: {question.explanation}
+                  </Typography>
+                  <Typography variant="body1">
+                    Knowledge: {question.knowledge}
+                  </Typography>
                   <br />
                 </div>
               ))}
             </div>
-            <Box mt={4}>
-              <Typography variant="h5" gutterBottom>
-                {getIconForAnswer(true)} What you know :
-              </Typography>
-              {questions.map((question, index) => (
-                <div key={index}>
-                  {storedAnswers &&
-                    storedAnswers[index] === question.correctAnswer && (
-                      <Typography variant="body1">
-                        Question {index + 1}: {question.question}
+            <div
+              className={`renderPagination-container ${
+                showResults ? "show" : ""
+              }`}
+            >
+              {showResults && renderPagination()}
+            </div>
+            <div className="pagination-container">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={toggleResults}
+              >
+                {showResults ? "Hide Results" : "View Results"}
+              </Button>
+            </div>
+            <div className={`results-container ${showResults ? "show" : ""}`}>
+              {showResults && getKnowQuestions().length > 0 && (
+                <div>
+                  <Typography variant="h5">
+                    {getIconForAnswer(true)}What You Know:
+                  </Typography>
+                  {getKnowQuestions().map((question) => (
+                    <div key={question.index}>
+                      <Typography variant="subtitle1">
+                        Question {question.index}: {question.question}
                       </Typography>
-                    )}
-                </div>
-              ))}
-            </Box>
-            <Box mt={4}>
-              <Typography variant="h5" gutterBottom>
-                {getIconForAnswer(false)} What you should Review :
-              </Typography>
-              {questions.map((question, index) => (
-                <div key={index}>
-                  {storedAnswers &&
-                    storedAnswers[index] !== undefined &&
-                    storedAnswers[index] !== question.correctAnswer && (
                       <Typography variant="body1">
-                        Question {index + 1}: {question.question}
+                        You Know About: {question.knowledge}
                       </Typography>
-                    )}
+                      <br />
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </Box>
+              )}
+              {showResults && getReviewQuestions().length > 0 && (
+                <div>
+                  <Typography variant="h5">
+                    {getIconForAnswer(false)} What You Should Review:
+                  </Typography>
+                  {getReviewQuestions().map((question) => (
+                    <div key={question.index}>
+                      <Typography variant="subtitle1">
+                        Question {question.index}: {question.question}
+                      </Typography>
+                      <Typography variant="body1">
+                        Review About: {question.knowledge}
+                      </Typography>
+                      <br />
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
           </CardContent>
         </Card>
       </Container>
