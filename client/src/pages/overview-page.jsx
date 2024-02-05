7a5b7d44d32a2648882cb50f2c315538fd5f4888
Kurt Weyne Gaso <94361651+Skamby@users.noreply.github.com>
2024-02-05 11:48:49 +0800

    Overview css and create exam css

--- client/src/pages/overview-page.jsx
+++ client/src/pages/overview-page.jsx
@@ -2,38 +2,18 @@ import React from "react";
 import { Grid, Paper, Typography } from "@mui/material";
 import Bar from "../components/Overview/Bar";
 import Pie from "../components/Overview/Pie";
-
+import "../assets/styles/overview-page.css";
 function OverviewAdmin() {
   return (
     <Grid container spacing={2}>
       <Grid item xs={6}>
-        <Paper
-          elevation={3}
-          style={{
-            padding: "10px",
-            height: "100%",
-            marginTop: "30px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
+        <Paper className="paper-pie" elevation={3}>
           <Typography variant="h6"></Typography>
           <Pie />
         </Paper>
       </Grid>
       <Grid item xs={6}>
-        <Paper
-          elevation={3}
-          style={{
-            padding: "10px",
-            height: "100%",
-            marginTop: "30px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
+        <Paper className="paper-bar" elevation={3}>
           <Typography variant="h6"></Typography>
           <Bar />
         </Paper>
